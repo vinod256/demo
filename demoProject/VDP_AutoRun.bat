2bfccd8af0ca088ce1cc74f512d6e48f4c7d1750
Vinod Kumar <VINOD1@discovery.co.za>
2017-07-04 11:13:02 +0200

    Bat file added

---
+++ demoProject/VDP_AutoRun.bat
@@ -0,0 +1,31 @@
+@echo off
+cd ..
+@echo off
+cd ..
+@echo off
+cd C:\Program Files (x86)\SmartBear\SoapUI-5.3.0\bin
+@echo off
+
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project.xml -s self_capture_reading
+
+call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project.xml -s all_vdp_services
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_New.xml -s gdl_autoRegister_linking_delinking
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_QA.xml -s Data_Uploads_Sleep
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_QA.xml -s self_capture_reading
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_qa.xml -s Data_Uploads_RealTime
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_qa.xml -s all_vdp_services
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_qa.xml -s Data_Uploads_Batch
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_qa.xml -s apple_health_vault
+
+
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_test.xml -s calories_calc_sevices
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_qa.xml -s Data_Uploads_Batch
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_test.xml -s Data_Uploads_Batch
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_qa.xml -s gdl_market_service_linking
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_qa.xml -s gdl_market_service_link_polar
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_qa.xml -s gdl_market_service_linking_xiaomi
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_qa.xml -s gdl_market_service_linking_ihealth
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_qa.xml -s gdl_autoRegister_linking_delinking
+::call testrunner.bat C:\Projects\GDL\SoapUI_Projects\VDP_Automation_Project_qa.xml -s gdl_autoRegister_linking_delinking
+
+
